fa6ac19e08bd31525365163cdab4f769164b0c29
Bao Nguyen <nnbao204@gmail.com>
2024-01-14 18:04:57 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import {createStackNavigator} from "@react-navigation/stack";
+// import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+// import {useFonts} from 'expo-font';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export default function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ed9de228b72228e2d6a18078b2919618`;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className = "App">
+
+    </div>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
